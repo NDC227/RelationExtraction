e4e3c0b01a23a3bb09d077d9d2623c88db4f574f
Andrew Chen <andrewchen227@yahoo.com>
2023-12-11 22:39:05 -0800

    added native torch training loop

--- relation_extraction.py
+++ relation_extraction.py
@@ -45,7 +45,7 @@ train_dataset = load_from_disk("datasets/train.hf")
 test_dataset = load_from_disk("datasets/test.hf")
 train_dataset.set_format(type="torch")
 test_dataset.set_format(type="torch")
-train_dataset = train_dataset.shuffle(seed=0).select(range(100))
+train_dataset = train_dataset.shuffle(seed=0).select(range(1000))
 test_dataset = test_dataset.shuffle(seed=0).select(range(500))
 train_dataset
 test_dataset
@@ -53,9 +53,10 @@ print(train_dataset)
 print(test_dataset)
 
 from transformers import AutoModelForSequenceClassification
-TRAIN = True
+TRAIN = False
 if TRAIN:
-    FREEZE_LAYERS = []
+    HF_TRAINER = False
+    FREEZE_LAYERS = ["bert.encoder"]
 
     model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=19)
     model.to(device)
@@ -64,42 +65,90 @@ if TRAIN:
     for name, param in model.named_parameters():
         if sum([name.startswith(layer) for layer in FREEZE_LAYERS]) > 0:
             param.requires_grad = False
-        print(name, param.requires_grad)
-
-    from transformers import TrainingArguments
-    training_args = TrainingArguments(per_device_train_batch_size=8,
-                                    per_device_eval_batch_size=8,
-                                    learning_rate=5e-5,
-                                    weight_decay=0,
-                                    num_train_epochs=3,
-                                    save_strategy="steps",
-                                    save_steps=100,
-                                    save_total_limit=3,
-                                    load_best_model_at_end=True,
-                                    optim="adamw_torch",
-                                    output_dir="trainer_ckpts", 
-                                    evaluation_strategy="steps",
-                                    eval_steps=100)
+        # print(name, param.requires_grad)
 
     import evaluate
-    metric = evaluate.load("accuracy")
 
-    def compute_metrics(eval_pred):
-        logits, labels = eval_pred
-        predictions = np.argmax(logits, axis=-1)
-        return metric.compute(predictions=predictions, references=labels)
+    # Train using HuggingFace Trainer
+    if HF_TRAINER:
+        from transformers import TrainingArguments
+        training_args = TrainingArguments(per_device_train_batch_size=8,
+                                        per_device_eval_batch_size=8,
+                                        learning_rate=5e-5,
+                                        weight_decay=0,
+                                        num_train_epochs=3,
+                                        save_strategy="steps",
+                                        save_steps=100,
+                                        save_total_limit=3,
+                                        load_best_model_at_end=True,
+                                        optim="adamw_torch",
+                                        output_dir="trainer_ckpts", 
+                                        evaluation_strategy="steps",
+                                        eval_steps=100)
 
-    from transformers import Trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        compute_metrics=compute_metrics,
-    )
+        metric = evaluate.load("accuracy")
 
-    trainer.train()
-    trainer.save_model(model_folder)
+        def compute_metrics(eval_pred):
+            logits, labels = eval_pred
+            predictions = np.argmax(logits, axis=-1)
+            return metric.compute(predictions=predictions, references=labels)
+
+        from transformers import Trainer
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            compute_metrics=compute_metrics,
+        )
+
+        trainer.train()
+        trainer.save_model(model_folder)
+
+    else:
+        from torch.utils.data import DataLoader
+        train_dataset = train_dataset.remove_columns(["text"])
+        test_dataset = test_dataset.remove_columns(["text"])
+        train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=8)
+        eval_dataloader = DataLoader(test_dataset, batch_size=8)
+
+        from torch.optim import AdamW
+        optimizer = AdamW(model.parameters(), lr=5e-5)
+
+        from transformers import get_scheduler
+        num_epochs = 3
+        num_training_steps = num_epochs * len(train_dataloader)
+        lr_scheduler = get_scheduler(
+            name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+        )
+
+        progress_bar = tqdm(range(num_training_steps))
+
+        model.train()
+        for epoch in range(num_epochs):
+            for batch in train_dataloader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                outputs = model(**batch)
+                loss = outputs.loss
+                loss.backward()
+
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+                progress_bar.update(1)
+        
+        metric = evaluate.load("accuracy")
+        model.eval()
+        for batch in eval_dataloader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            with torch.no_grad():
+                outputs = model(**batch)
+
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+            metric.add_batch(predictions=predictions, references=batch["labels"])
+
+        metric.compute()
 
 model = AutoModelForSequenceClassification.from_pretrained("./" + model_folder).to(device)
 untrained_model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=19)
